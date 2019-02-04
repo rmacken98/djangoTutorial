9a142f69fe1cd34f6f725aec2170e8d128fe07ce
Ronan Macken <C16444114@mydit.ie>
2019-02-04 17:41:31 +0000

    tutorial 4

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,31 +1,24 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
+class IndexView(generic.ListView):
+    template_name= 'polls/index.html'
+    context_object_name= 'latest_question_list'
 
-def index(request):
-    #return HttpResponse("Hello, world. You're at the polls index.")
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-   # template = loader.get_template('polls/index.html')
-    context = {'latest_question_list' : latest_question_list}
-    #output =', '.join([q.question_text for q in latest_question_list])
-    #return HttpResponse(template.render(context,request))
-    return render(request,'polls/index.html', context)
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def detail(request,question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-    # return HttpResponse("You're looking at question %s." % question_id)
-    #try:
-      # question= Question.objects.get(pk=question_id)
-    #except Question.DoesNotExist:
-      # raise Http404('Question does not exist')
-   # return render(request, 'polls/detail.html',{'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name='polls/detail.html'
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name= 'polls/results.html'
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
