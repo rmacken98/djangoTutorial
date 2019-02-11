1603d46016439df7c75c76aa02fa2d89045afcdc
Ronan Macken <C16444114@mydit.ie>
2019-02-11 15:06:49 +0000

    tutorial part 6

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,26 +2,31 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
-from django.utils import timezone
+import datetime
+
 
 from .models import Choice, Question
+
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
-def get_queryset(self):
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-    return Question.objects.filter(
-        pub_date__lte=timezone.now()
-    ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
-    template_name='polls/detail.html'
+    template_name = 'polls/detail.html'
+
 
 class ResultsView(generic.DetailView):
     model = Question
-    template_name= 'polls/results.html'
+    template_name = 'polls/results.html'
+
+    def get_queryset(self):
+      return Question.objects.filter(pub_date__lte=timezone.now())
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
